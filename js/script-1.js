310c07e81b13871b6d46ede54d729b1635fad382
MRErnest-tech <316255589@pcpuma.acatlan.unam.mx>
2022-03-23 10:36:11 -0600

    Practica JavaScript

---
+++ js/script-1.js
@@ -0,0 +1,12 @@
+
+function cambio1() {
+    var inputImage= document.getElementById("image");
+    inputImage.src= "img/image-1.jpeg";
+
+}
+
+function cambio2(){
+    var inputImage= document.getElementById("image");
+    inputImage.src= "img/image-2.jpeg";
+
+}
